05258e85307dfb23a980c8ee00e095b2e1c8973f
babrath <bert.abrath@UGent.be>
2017-09-12 21:11:06 +0200

    Put an assertion on SPEC testing.

--- core/spec.py
+++ core/spec.py
@@ -15,4 +15,4 @@ def test(binary, test_dir, config):
     # Execute the regression script
     conf_file = os.path.join(os.path.dirname(config.default['input_source_directory']), 'spec2006_test.conf')
     result = subprocess.check_output([regression_script, '-c', conf_file, '-T', test_dir, '-d', fake_diablo_dir, '-p', fake_diablo_bin, config.default['binary_name']], stderr=subprocess.DEVNULL, universal_newlines=True).splitlines()[-1]
-    print(result)
+    assert result == 'OK', 'SPEC regression script failed!'
