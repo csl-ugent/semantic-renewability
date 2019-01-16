8a2f0deec670a34d5d44957a13aa7d37d5ff9efe
babrath <bert.abrath@UGent.be>
2019-01-16 16:43:18 +0100

    Allow output directory to be overridden.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import executor.executor as executor
 # Debugging format.
 DEBUG_FORMAT = '%(levelname)s:%(filename)s:%(funcName)s:%(asctime)s %(message)s\n'
 
-def main(mode, number_of_seeds, numbers_of_versions, seed, testmode, transformation_type):
+def main(mode, number_of_seeds, numbers_of_versions, output_dir, seed, testmode, transformation_type):
     logging.debug('Executing...')
 
     # Change the directory
@@ -43,7 +43,7 @@ def main(mode, number_of_seeds, numbers_of_versions, seed, testmode, transformat
     seed = seed if seed else int(config_obj.semantic_mod['seed'])
     seeds = range(seed, seed + number_of_seeds) if number_of_seeds else [seed]
 
-    output_dir_config = config_obj.default['output_directory']
+    output_dir_config = output_dir if output_dir else config_obj.default['output_directory']
     shutil.rmtree(output_dir_config, True)
     for seed in seeds:
         config_obj.semantic_mod['seed'] = str(seed)
@@ -84,6 +84,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debugging log.')
     parser.add_argument('-m', '--mode', type=int, default=2, help='The mode in which the framework is to be executed.')
     parser.add_argument('-n', '--number_of_seeds', type=int, help='The number of seeds to test.')
+    parser.add_argument('-o', '--output_dir', help='The output directory.')
     parser.add_argument('-s', '--seed', type=int, help='The seed.')
     parser.add_argument('-t', '--testmode', type=int, default=0, help='The mode in which testing is to happen. 0 is no testing.')
     parser.add_argument('-v', '--numbers_of_versions', type=str, help='The numbers of versions to test.')
@@ -102,4 +103,4 @@ if __name__ == '__main__':
         rootLogger.addHandler(fileHandler)
 
     # Start the execution.
-    main(args.mode, args.number_of_seeds, args.numbers_of_versions, args.seed, args.testmode, args.transformation_type)
+    main(args.mode, args.number_of_seeds, args.numbers_of_versions, args.output_dir, args.seed, args.testmode, args.transformation_type)
